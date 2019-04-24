158eb797afbafbbbd3c9e83c3c56116df9d25984
fosslinux <fosslinux@tilde.team>
2019-04-24 14:58:12 +1000

    refractor

--- fmt-md-table.py
+++ fmt-md-table.py
@@ -11,34 +11,36 @@ def parse_input():
     return table
 
 def format_input(table):
-    for index in range(len(table)):
-        table[index] = table[index].rstrip()
-        if not table[index]:
+    for index, row in enumerate(table):
+        row = row.rstrip()
+        if not row:
+            # deletes the actual table index
             del table[index]
-        line = table[index]
-        table[index] = []
-        for column in line.split('|'):
-            table[index].append(column)
-        del table[index][0]
-        del table[index][-1]
+            continue
+        new_row = []
+        # slice to remove first and last indexes
+        for column in row.split('|')[1:-1]:
+            new_row.append(column)
+        # assign our new row to the table
+        table[index] = new_row
     return table
 
 def get_column_length(table):
     columns_num = len(table[0])
     column_length = [0 for __ in range(columns_num)]
-    for line in table:
-        if len(line) != columns_num:
-            print("invalid syntax!")
+    for row in table:
+        if len(row) != columns_num:
+            print("rows have different number of columns!", file=sys.stderr)
             print(line)
             sys.exit(1)
-        for index, column in enumerate(line):
+        for index, column in enumerate(row):
             if len(column) > column_length[index]:
                 column_length[index] = len(column)
     return column_length
 
 def pad_table(table, column_length):
-    for table_index, line in enumerate(table):
-        for column_index, column in enumerate(line):
+    for table_index, row in enumerate(table):
+        for column_index, column in enumerate(row):
             if column_length[column_index] > len(column):
                 padding = column_length[column_index] - len(column)
                 for __ in range(padding):
@@ -46,10 +48,11 @@ def pad_table(table, column_length):
     return table
 
 def print_table(table):
-    for table_index in range(len(table)):
+    for row in table:
         print("|", end='')
-        for column_index in range(len(table[table_index])):
-            print(f"{table[table_index][column_index]}|", end='')
+        for col in row:
+            print(col + "|", end='')
+        # newline for EOL
         print()
 
 def main():
